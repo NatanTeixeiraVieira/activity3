997639d5047c923488bd7f5b335f135138d5ca44
Dyeison-Felipe <dyeisonfc@outlook.com>
2025-03-06 07:01:26 -0300

    feat: create function login

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -19,6 +19,9 @@ class Register {
 
     public function setName($name) {
         if (!empty($name) && is_string($name) && strlen($name) > 2) {
+            if (!preg_match('/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/', $name)) {
+                throw new Exception("Nome inválido. Apenas letras e espaços são permitidos.");
+            }
             $this->name = $name;
         } else {
             throw new Exception("Nome inválido. Deve ter pelo menos 3 caracteres.");
@@ -33,7 +36,7 @@ class Register {
         if (!empty($username) && preg_match('/^[a-zA-Z0-9_]+$/', $username)) {
             $this->username = $username;
         } else {
-            throw new Exception("Nome de usuário inválido. Apenas letras, números e underscore são permitidos.");
+            throw new Exception("Username de usuário inválido. Apenas letras, números e underscore são permitidos.");
         }
     }
 
@@ -70,5 +73,34 @@ class Register {
         file_put_contents($file, $data, FILE_APPEND); // Adiciona os dados ao arquivo
 
         echo "Usuário salvo com sucesso! ID: {$this->id}\n";
+    }
+
+    public function login($username, $password) {
+        $file =  __DIR__."/../cadastro/users.txt";
+
+        if(!file_exists($file)){
+            throw new Exception("Arquivo não encontrado");
+        }
+
+        // Lê o conteúdo do arquivo e retorna um array onde cada linha é um elemento.
+        // FILE_IGNORE_NEW_LINES → Remove quebras de linha (\n)
+        // FILE_SKIP_EMPTY_LINES → Ignora linhas vazias.
+        $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach($lines as $line) {
+            list($id, $name, $storedUsername, $hashPassword) = explode(';', $line);
+
+          // verifica se o username é o mesmo
+          if($storedUsername == $username){
+            //verifica se a senha corresponde a senha armazenada
+            if(password_verify($password, $hashPassword)){
+                echo "Login bem sucedido!";
+                return true;
+            } else {
+                throw new Exception("Senha incorreta.");
+            }
+          }  
+        }
+        thro new Exception("Usuário não encontrado")
     }
 }
